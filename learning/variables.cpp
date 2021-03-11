2c53af3e39742850613201cc61ffd3948fc3723e
beomus <trunghau60@gmail.com>
2021-03-11 11:10:28 +0900

    first commit

---
+++ learning/variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // variable assignment start with type
+    string name = "bruv";
+
+    // does not have to assign immediately
+    int age;
+    age = 3;
+    cout << "hello " << name << endl;
+    cout << age << endl;
+    return 0;
+}
