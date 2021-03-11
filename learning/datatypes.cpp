5a91b4ebe22988483ed74658cad63d933502d013
beomus <trunghau60@gmail.com>
2021-03-11 11:25:57 +0900

    added datatypes and strings

---
+++ learning/datatypes.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // char: character datatype, using single quote for a single character
+    char var1 = 'A';
+
+    // string: sequence of char, using a double quote
+    string var2 = "A bunch of different characters.";
+
+    // int: integer
+    int var3 = 10;
+
+    // float: float
+    float var4 = 1.5;
+
+    // double: more decimal points than float
+    double var5 = 1.230;
+
+    // boolean: well boolean, all lowercase
+    bool var6 = true;
+
+    return 0;
+}
---
+++ learning/strings.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string phrase = "what what";
+
+    cout << "hello";
+    cout << "my name\n";
+    cout << "what?" << endl;
+    cout << phrase.length();
+
+    return 0;
+}
