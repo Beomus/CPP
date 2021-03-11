2ff836d67d68c45667c16cfcdf52181d5992fa62
beomus <trunghau60@gmail.com>
2021-03-11 11:34:04 +0900

    fixed typo string

--- learning/strings.cpp
+++ learning/strings.cpp
@@ -5,11 +5,27 @@ using namespace std;
 int main() {
 
     string phrase = "what what";
-
+    
+    // different output
     cout << "hello";
     cout << "my name\n";
     cout << "what?" << endl;
-    cout << phrase.length();
+
+    // get length of string
+    cout << phrase.length() << endl;
+
+    // indexing
+    cout << phrase[3] << endl;
+    phrase[2] = ' ';
+    cout << phrase << endl;
+
+    // find(string/char, int: starting ind)
+    cout << phrase.find("at") << endl;
+
+    // substr(string, int: ind, length: int)
+    string phraseSub;
+    phraseSub = phrase.substr(2, 5);
+    cout << phraseSub;
 
     return 0;
 }
