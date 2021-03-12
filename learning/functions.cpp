dd5a560fd7743c9d330697533026894a60c27264
Beomus <trunghau60@gmail.com>
2021-03-12 13:39:01 +0900

    fixed functions

--- learning/functions.cpp
+++ learning/functions.cpp
@@ -4,18 +4,20 @@ using namespace std;
 
 /*
 start with a return data type
-then name
-
+then name and parameters
 */
 
-// void returns nothing
-void sayHi(string name)
-{
-    cout << "hello " << name << endl;
-}
+// create an empty reference for functions
+void sayHi(string name);
 
 int main()
 {
     sayHi("bruh");
     return 0;
+}
+
+// void returns nothing
+void sayHi(string name)
+{
+    cout << "hello " << name << endl;
 }
